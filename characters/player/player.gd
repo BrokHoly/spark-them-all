c27b989c4066dc69c77cae06e57f7047af65ab0c
bbricard <baptistebricard@etudiant.univ-rennes.fr>
2026-02-11 15:29:19 +0100

    Just things to go right

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -1,13 +1,23 @@
 extends CharacterBody3D
 
-
+# Relative to player control
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
-const LOOK_SENSITIVITY = 0.005
+const LOOK_SENSITIVITY = 0.002
+var isFPS: bool = true
+
+# Relative to the lamp
+const LAMP_IDLE_ANGLE = 45
+const LAMP_IDLE_RANGE = 5
+const LAMP_FOCUS_ANGLE = 30
+const LAMP_FOCUS_RANGE = 10
 
 @onready var camera: Node3D = $FPSAnchor
+@onready var lamp: Node3D = $LampAnchor
 
-var isFPS: bool = true
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -32,9 +42,17 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		print(event.relative)
+		if event.is_action_pressed("pause"):
+			get_tree().quit()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * LOOK_SENSITIVITY) # Turn juste le body
+		
 		camera.rotate_x(-event.relative.y * LOOK_SENSITIVITY) # Turn l'ancre de pivot de FPS
 		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2) # La limite à 90 -90 deg 
+		
+		var target_lamp_x = clamp(camera.rotation.x, -PI/4, PI/4)
+		lamp.rotation.x = lerp(lamp.rotation.x, target_lamp_x, 0.15)
+
 		

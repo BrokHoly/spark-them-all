a8997e84c1501d574a8ad03d6f44e1a53a6199ab
BrokHoly <62349056+BrokHoly@users.noreply.github.com>
2026-02-23 08:46:03 +0100

    Load a protohub when starting

--- characters/enemies/boular/boular.gd
+++ characters/enemies/boular/boular.gd
@@ -9,9 +9,37 @@ var time_since_last_hit = 0.0
 
 var grape_scene : PackedScene = preload("res://collectibles/loots/boular_grape.tscn")
 
+@export var player: Node3D 
+
 func _ready() -> void:
 	killed.connect(player.on_ennemy_killed)
 
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	time_since_last_hit += delta
+	
+	if player:
+		var to_player = (player.global_position - global_position)
+		to_player.y = 0  # Ignore vertical differences
+		if to_player.length() > 0.1:
+			var direction = to_player.normalized()
+			
+			# Movement
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+			
+			# Smooth rotation toward player
+			var target_rotation = Vector3(0, atan2(-direction.x, -direction.z), 0)
+			rotation.y = lerp_angle(rotation.y, target_rotation.y, 5.0 * delta)
+		else:
+			# Slow down if close
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+	move_and_slide()
+	
 func expose_to_light(delta: float, damage_per_tick: int, damage_cooldown: float):
 	# Only take damage if cooldown passed
 	if time_since_last_hit >= damage_cooldown:
